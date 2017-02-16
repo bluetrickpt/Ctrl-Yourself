8b5a5c18a05fe275809c884863fe6ee83bc10ae7
Ricardo Da Silva Carvalho Mendes <rscmendes@dei.uc.pt>
2017-02-16 23:59:31 +0000

    Demo keyboard listener with jnativehook working

---
+++ src/main/java/com/whatthehack/ctrl/yourself/Main.java
@@ -0,0 +1,22 @@
+package com.whatthehack.ctrl.yourself;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+
+/**
+ * 
+ */
+public class Main extends JFrame {
+
+	/**
+         * 
+	 */
+	public static void main(String[] args) {
+            SwingUtilities.invokeLater(new Runnable() {
+                    public void run() {
+                            new NativeHookDemo();
+                    }
+            });
+	}
+}
